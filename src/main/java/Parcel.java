1a26ee2fc6dad7ce5ca8d5c7905a283e0b9c80ff
Anna Becker and Abigail Rolling <anna.r.becker@gmail.com>
2016-02-15 11:14:45 -0800

    Instantiate Parcel

---
+++ src/main/java/Parcel.java
@@ -0,0 +1,18 @@
+public class Parcel {
+
+  private double mLength;
+  private double mWidth;
+  private double mHeight;
+  private double mWeight;
+
+  public Parcel(double length, double width, double height, double weight) {
+    mLength = length;
+    mWidth = width;
+    mHeight = height;
+    mWeight = weight;
+  }
+
+  public double volume() {
+    return 1.0;
+  }
+}
--- src/test/java/ParcelTest.java
+++ src/test/java/ParcelTest.java
@@ -2,5 +2,16 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ParcelTest {
-  
+
+  @Test
+  public void createNewParcel_instantiatesCorrectly() {
+    Parcel testParcel = new Parcel(3.0, 3.0, 3.0, 7.0);
+    assertEquals(true, testParcel instanceof Parcel);
+  }
+
+  @Test
+  public void volume_calculateVolumeWithGivenDimensions_parcelVolume() {
+    Parcel testParcel = new Parcel(3.0, 4.0, 5.0, 12.5);
+    assertEquals(60.0, testParcel.volume());
+  }
 }
